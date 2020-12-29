3d79095ee80292e125c2b66863bab7b39e21f180
Su Anli <sual0001@gmail.com>
2020-12-29 19:28:56 +0800

    chore(babel, package, tsconfig): add path alias @screens

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {HomeScreen} from '@screens';
 import React from 'react';
-import {HomeScreen} from './screens';
 
 const Stack = createStackNavigator();
 
