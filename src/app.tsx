f5c79554a0862d447a20d85621221e1238b365a4
Su Anli <sual0001@gmail.com>
2020-12-29 19:31:01 +0800

    chore(react-native-paper): add library

--- src/app.tsx
+++ src/app.tsx
@@ -2,20 +2,23 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {HomeScreen} from '@screens';
 import React from 'react';
+import {Provider as PaperProvider} from 'react-native-paper';
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen.Component}
-          options={HomeScreen.options}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen.Component}
+            options={HomeScreen.options}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 };
 
