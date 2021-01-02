ffece20c9ad85c8927d99f0d96ea4f72155d0f5b
Su Anli <sual0001@gmail.com>
2021-01-02 17:27:11 +0800

    feat(play-screen, play-slice): add players ui for one player

--- src/screens/play/play.tsx
+++ src/screens/play/play.tsx
@@ -1,26 +1,55 @@
 import {useNavigation} from '@react-navigation/native';
 import {StackNavigationOptions} from '@react-navigation/stack';
-import {menuItemsMapSelector, playersMapSelector} from '@store';
+import {PlaySelectors} from '@store';
 import {colors} from '@theme';
 import R from 'ramda';
 import React from 'react';
-import {FAB} from 'react-native-paper';
+import {View} from 'react-native';
+import {Appbar, FAB} from 'react-native-paper';
 import {useSelector} from 'react-redux';
 import styled from 'styled-components/native';
+import Player from './player';
 
 const Component = () => {
   const navigation = useNavigation();
-  const menuItemsMap = useSelector(menuItemsMapSelector);
-  const playersMap = useSelector(playersMapSelector);
+  const menuItemsMap = useSelector(PlaySelectors.menuItemsMap);
+  const playersMap = useSelector(PlaySelectors.playersMap);
+  const totalScoresMap = useSelector(PlaySelectors.totalScoresMap);
 
   const start = () => {
     navigation.navigate('PlayerAddScreen');
   };
 
   const hasGame = !R.isEmpty(menuItemsMap) && !R.isEmpty(playersMap);
+  const players = Object.keys(playersMap).map((playerName) => ({
+    id: playerName,
+    name: playerName,
+    color: 'white',
+  }));
 
   return (
     <Screen>
+      {hasGame && (
+        <>
+          <AppBarHeader>
+            <Appbar.Content title="Sushi Go Party!" />
+          </AppBarHeader>
+
+          <View>
+            <Players horizontal showsHorizontalScrollIndicator={false}>
+              {players.map(({id, name, color}) => (
+                <Player
+                  key={id}
+                  name={name}
+                  score={totalScoresMap?.[id] || 0}
+                  color={color}
+                  selected={true}
+                />
+              ))}
+            </Players>
+          </View>
+        </>
+      )}
       {!hasGame && (
         <StartButton testID="StartButton" icon="plus" onPress={start} />
       )}
@@ -47,4 +76,14 @@ const StartButton = styled(FAB)`
   right: 0px;
   bottom: 0px;
   background-color: ${colors.primary};
+`;
+
+const AppBarHeader = styled(Appbar.Header)`
+  padding-left: 10px;
+  padding-right: 10px;
+  background-color: transparent;
+`;
+
+const Players = styled.ScrollView`
+  padding-left: 12px;
 `;
