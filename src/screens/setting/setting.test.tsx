963d2520d40fab2020780cde065fd95df6132d06
Su Anli <anli@users.noreply.github.com>
2021-01-17 23:13:01 +0800

    feat(settings): add in app review (#81)
    closes #73

--- src/screens/setting/setting.test.tsx
+++ src/screens/setting/setting.test.tsx
@@ -1,13 +1,13 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import store from '@store';
-import {render} from '@testing-library/react-native';
+import {act, fireEvent, render} from '@testing-library/react-native';
 import React from 'react';
 import deviceInfoModule from 'react-native-device-info';
+import InAppReview from 'react-native-in-app-review';
 import {Provider as PaperProvider} from 'react-native-paper';
 import {Host} from 'react-native-portalize';
 import {Provider as ReduxProvider} from 'react-redux';
-import {act} from 'react-test-renderer';
 import SettingScreen from './setting';
 
 const App = ({component, options}: any) => {
@@ -47,7 +47,9 @@ describe('Setting Screen', () => {
       When I am at Setting Screen
       Then I should see 'Settings'
       And I should see 'Version'
-      And I should see '1.0.0'`, async () => {
+      And I should see '1.0.0'
+      And I should see 'Send a feedback or review'
+      And I should see 'Thank you for your support!'`, async () => {
     const {getByText} = render(
       <App
         component={SettingScreen.Component}
@@ -59,5 +61,25 @@ describe('Setting Screen', () => {
     expect(getByText('Settings')).toBeDefined();
     expect(getByText('Version')).toBeDefined();
     expect(getByText('1.0.0')).toBeDefined();
+    expect(getByText('Send a feedback or review')).toBeDefined();
+    expect(getByText('Thank you for your support!')).toBeDefined();
+  });
+
+  it(`Scenario: Send feedback
+      Given that I am at Setting Screen
+      When I press 'Send a feedback or review'
+      Then I should see 'In App Review'`, async () => {
+    const spyRequestInAppReview = jest.spyOn(InAppReview, 'RequestInAppReview');
+    const {getByText} = render(
+      <App
+        component={SettingScreen.Component}
+        options={SettingScreen.options}
+      />,
+    );
+    await act(async () => {
+      fireEvent.press(getByText('Send a feedback or review'));
+    });
+
+    expect(spyRequestInAppReview).toHaveBeenCalledTimes(1);
   });
 });
