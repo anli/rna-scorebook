44d143986f53191938fdba0b5013210768aee55d
Su Anli <sual0001@gmail.com>
2021-01-17 14:42:48 +0800

    feat(app): add checkUpdateNeeded
    closes #58

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,12 @@
 import Navigation from '@navigation';
 import store, {persistor} from '@store';
-import React from 'react';
-import {StatusBar} from 'react-native';
+import React, {useEffect} from 'react';
+import {Alert, BackHandler, Linking, StatusBar} from 'react-native';
 import {
   DefaultTheme as PaperDefaultTheme,
   Provider as PaperProvider,
 } from 'react-native-paper';
+import VersionCheck from 'react-native-version-check';
 import {Provider as ReduxProvider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import colors from './theme/colors';
@@ -18,6 +19,32 @@ const theme = {
 };
 
 const App = () => {
+  useEffect(() => {
+    checkUpdateNeeded();
+  }, []);
+
+  const checkUpdateNeeded = async () => {
+    try {
+      let updateNeeded = await VersionCheck.needUpdate();
+      if (updateNeeded.isNeeded) {
+        Alert.alert(
+          'Please update',
+          'You will have to update your app to the latest version to continue using.',
+          [
+            {
+              text: 'Update',
+              onPress: () => {
+                BackHandler.exitApp();
+                Linking.openURL(updateNeeded.storeUrl);
+              },
+            },
+          ],
+          {cancelable: false},
+        );
+      }
+    } catch {}
+  };
+
   return (
     <ReduxProvider store={store}>
       <PersistGate loading={null} persistor={persistor}>
