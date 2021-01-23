58bed0df8cdf89745fbf431bb4ff7ab9340835b2
Su Anli <sual0001@gmail.com>
2021-01-23 14:35:20 +0800

    test(summary.test): fix test coverage

--- src/screens/summary/summary.test.tsx
+++ src/screens/summary/summary.test.tsx
@@ -1,6 +1,6 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import store, {gameSlice} from '@store';
+import store from '@store';
 import {render} from '@testing-library/react-native';
 import React from 'react';
 import deviceInfoModule from 'react-native-device-info';
@@ -24,7 +24,14 @@ jest.mock('react-native-share', () => ({
   open: jest.fn(),
 }));
 
-const App = ({component, options}: any) => {
+const defaultParams = {
+  startDate: undefined,
+  playerRankings: [],
+  gameName: 'GAME_NAME',
+  headers: [],
+};
+
+const App = ({component, options, initialParams = {}}: any) => {
   const Stack = createStackNavigator();
 
   return (
@@ -37,6 +44,7 @@ const App = ({component, options}: any) => {
                 name="App"
                 component={component}
                 options={options}
+                initialParams={initialParams}
               />
             </Stack.Navigator>
           </Host>
@@ -58,30 +66,45 @@ describe('Summary Screen', () => {
   it(`Scenario: See Summary Screen
       Given any      
       When I am at Summary Screen
-      Then I should see 'Top Sushi Go-ers!'`, async () => {
+      Then I should see 'Best of GAME_NAME'`, async () => {
     const {getByText} = render(
       <App
         component={SummaryScreen.Component}
         options={SummaryScreen.options}
+        initialParams={defaultParams}
       />,
     );
     await act(async () => {});
 
-    expect(getByText('Top Sushi Go-ers!')).toBeDefined();
+    expect(getByText('Best of GAME_NAME')).toBeDefined();
   });
 
   it(`Scenario: 3 Players
       Given that I have 3 players 'ME', 'John' 'Mary'
       When I am at Summary Screen
       Then I should see 'Top Three Player' 'ME' 'John' 'Mary'`, async () => {
-    store.dispatch(gameSlice.actions.startGame(defaultMenuItemIds));
-    store.dispatch(gameSlice.actions.addPlayer('John'));
-    store.dispatch(gameSlice.actions.addPlayer('Mary'));
+    const categories = [
+      {name: '1', categoryName: '1', value: '0', isNumeric: true},
+      {name: '2', categoryName: '2', value: '50'},
+    ];
+    const playerRankings = [
+      {categories, id: 'ME', name: 'ME', rank: 1, totalScore: '0'},
+      {categories, id: 'John', name: 'John', rank: 2, totalScore: '50'},
+      {categories, id: 'Mary', name: 'Mary', rank: 3, totalScore: '100'},
+    ];
+    const headers = [{title: 'Player'}, {title: 'Total', isNumeric: true}];
+    const params = {
+      ...defaultParams,
+      startDate: new Date(),
+      playerRankings,
+      headers,
+    };
 
     const {getByTestId} = render(
       <App
         component={SummaryScreen.Component}
         options={SummaryScreen.options}
+        initialParams={params}
       />,
     );
     await act(async () => {});
